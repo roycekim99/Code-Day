eccf6c57645b7f076e0c42537f7cb3be34a72747
Royce <roycekim99@gmail.com>
2019-02-16 14:18:04 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function animate() {
+    document.querySelector('blockquote').classList.toggle('animate');
+  }
+  
+  document.addEventListener('click', animate);
+  
+  setTimeout(animate, 1000);
